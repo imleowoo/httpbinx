77a7253ef693683d1b469022167735c013c28b82
wulei <wulei@youmi.net>
2022-03-16 18:52:50 +0800

    feat(router): add Dynamic data - base64, delay

--- httpbin/routers/dynamicdata.py
+++ httpbin/routers/dynamicdata.py
@@ -2,49 +2,83 @@
 """Dynamic Data"""
 import asyncio
 import base64
+import random
 import uuid
 
 from fastapi import APIRouter
 from fastapi import Path
+from fastapi import Query
 from fastapi.responses import PlainTextResponse
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+from httpbin.constants import AWESOME_HTTPBIN_BASE64ENCODED
 from httpbin.helpers import get_request_attrs
+from httpbin.responses import OctetStreamResponse
+from httpbin.schemas import RequestDictModel
 
 router = APIRouter()
 
 
-@router.get('/base64/{value}')
-async def decode_base64(value: str):
+@router.get(
+    '/base64/{value}',
+    response_class=PlainTextResponse,
+    description='Decodes base64url-encoded string.',
+)
+async def decode_base64(
+        value: str = Path(
+            ...,
+            title='base64-encoded string',
+            example=AWESOME_HTTPBIN_BASE64ENCODED
+        )
+):
     encoded: bytes = value.encode('utf-8')
     try:
         decoded = base64.urlsafe_b64decode(encoded).decode('utf-8')
         return PlainTextResponse(content=decoded)
     except Exception as err:
         return PlainTextResponse(
-            content=f'Incorrect Base64 data try: {value}, err_msg: {err}',
+            content=f'Incorrect Base64 data: {value}, err_msg: {err}',
             status_code=status.HTTP_400_BAD_REQUEST
         )
 
 
-@router.get('/bytes/{n}')
-async def random_bytes(n: int):
+@router.get(
+    '/bytes/{n}',
+    response_class=OctetStreamResponse,
+    description='Returns n random bytes generated with given seed'
+)
+async def random_bytes(
+        n: int = Path(..., title='binary file size', gt=0),
+        seed: int = Query(
+            None,
+            title='random seed',
+            description='Initialize the random number generator'
+        )
+):
     # set 100KB limit
-    pass
+    n = min(n, 100 * 1024)
+    if seed is not None:
+        random.seed(seed)
+    # Note: can't just use os.urandom here because it ignores the seed
+    # https://docs.python.org/3/library/random.html?highlight=random%20seed#random.seed
+    content = bytes(random.randint(0, 255) for _ in range(n))
+    return OctetStreamResponse(content=content)
 
 
 @router.api_route(
     '/delay/{delay}',
-    methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'TRACE']
+    methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'TRACE'],
+    response_model=RequestDictModel,
+    description='Returns a delayed response (max of 10 seconds).',
+    response_description='A delayed response.'
 )
 async def delay_response(
         *,
-        delay: int = Path(..., ge=0, le=10),
+        delay: float = Path(..., ge=0, le=10, description='delay seconds'),
         request: Request
 ):
-    """Returns a delayed response."""
     await asyncio.sleep(delay)
     return get_request_attrs(
         request,
