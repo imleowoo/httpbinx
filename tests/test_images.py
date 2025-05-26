fdc8621f8e0f9eeda3465bd40256913ead9b9a19
Leo <imleowoo@outlook.com>
2025-05-26 16:37:43 +0800

    fix: Fix the error of not finding the images file. (#61)

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tag: Images
 """
@@ -9,16 +8,31 @@ from httpbinx import app
 client = TestClient(app)
 
 
-def test_image(): pass
+def test_image():
+    response = client.get('/image')
+    assert response.status_code == 200
+    assert response.headers['content-type'].startswith('image')
 
 
-def test_image_png(): pass
+def test_image_png():
+    response = client.get('/image/png')
+    assert response.status_code == 200
+    assert response.headers['content-type'].startswith('image/png')
 
 
-def test_image_jpeg(): pass
+def test_image_jpeg():
+    response = client.get('/image/jpeg')
+    assert response.status_code == 200
+    assert response.headers['content-type'].startswith('image/jpeg')
 
 
-def test_image_webp(): pass
+def test_image_webp():
+    response = client.get('/image/webp')
+    assert response.status_code == 200
+    assert response.headers['content-type'].startswith('image/webp')
 
 
-def test_image_svg(): pass
+def test_image_svg():
+    response = client.get('/image/svg')
+    assert response.status_code == 200
+    assert response.headers['content-type'].startswith('image/svg')
