dd2e10959734f85f71659ffefb4eb8881f046203
Leo <imleowoo@outlook.com>
2025-06-09 13:07:34 +0800

    feat: The newly added API /bombs has implemented the functionality of a bomb for compressed files. (#62)
    * feat(routers.anything): add a Zip Bombs api
    
    * fix: Fix the error of not finding the images file. (#61)
    
    * fix: Fix the error of not finding the images file. (#61)
    
    * feat: The newly added API /bombs has implemented the functionality of a bomb for compressed files.
    
    * test: Add unit tests for the bombs

--- httpbinx/routers/anything.py
+++ httpbinx/routers/anything.py
@@ -1,11 +1,23 @@
-# -*- coding: utf-8 -*-
-from fastapi import APIRouter
-from starlette.requests import Request
+from enum import Enum
+from os import path
 
-from httpbinx.helpers import to_request_info
+from fastapi import APIRouter, Path
+from starlette.requests import Request
+from starlette.responses import FileResponse
+
+from httpbinx.helpers import get_bomb_file_path, to_request_info
 from httpbinx.schemas import RequestInfo
 
 router = APIRouter(tags=['Anything'])
+
+
+class BombTypes(str, Enum):
+    """The allowed bomb file compression types are: brotli and gzip."""
+    brotli = 'brotli'
+    gzip = 'gzip'
+
+
+bombs_path: Path = get_bomb_file_path()
 
 
 @router.api_route(
@@ -17,3 +29,34 @@ router = APIRouter(tags=['Anything'])
 )
 async def anything(request: Request):
     return await to_request_info(request)
+
+
+@router.get(
+    '/bombs/{file}',
+    response_class=FileResponse,
+    summary='Returns a bomb file.',
+    description='**It may cause your client to crash! '
+                'References [I use Zip Bombs to Protect my Server](https://idiallo.com/blog/zipbomb-protection)**',
+    response_description='Return a bomb for compressed files.'
+)
+async def bomb_file(
+        *,
+        file: BombTypes = Path(
+            ...,
+            title='Compression types',
+            description='The allowed bomb file compression types',
+        )
+):
+    if file == BombTypes.gzip:
+        # dd if=/dev/zero bs=1M count=1000 | gzip -c > bomb-1GB.gz
+        return FileResponse(
+            path=path.join(bombs_path, 'bomb-1GB.gz'),
+            headers={'Content-Encoding': 'gzip'},
+        )
+    elif file == BombTypes.brotli:
+        # dd if=/dev/zero bs=1M count=1000 | brotli > bomb-1GB.br
+        return FileResponse(
+            path=path.join(bombs_path, 'bomb-1GB.br'),
+            headers={'Content-Encoding': 'br'},
+        )
+    raise ValueError(f'{file} is not a valid file type.')
