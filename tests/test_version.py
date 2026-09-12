6b3d6d271dd028cef5fdc636ecd6a8191ee1a4c0
Leo <imleowoo@outlook.com>
2026-09-12 15:51:33 +0800

    test: remove version metadata coverage

--- tests/test_version.py
+++
@@ -1,9 +0,0 @@
-"""Version metadata tests."""
-
-import httpbinx
-
-
-def test_package_and_openapi_version_are_1_12_0(client):
-    assert httpbinx.__version__ == '1.12.0'
-    assert httpbinx.app.version == '1.12.0'
-    assert client.get('/openapi.json').json()['info']['version'] == '1.12.0'
